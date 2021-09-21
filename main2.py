27edf2d1541f9356245ef46406cbfc3e62cdf7c4
pizzadeesnutz <arcadio@thearcadio.com>
2021-09-21 11:14:30 -0400

    Saving my minor changes
    Made a few minor changes to my main class but pulling in the main branch again so I can see changes

--- main2.py
+++ main2.py
@@ -6,17 +6,32 @@ from maze import Maze
 
 def main():
     # Q6
-    timeM = 0
+    timeM = 0 # time for Manhattan distance
+    atl = 0 # average trajectory length
+    spDisGW = 0
+    spFullGW = 0
+    astarCells = 0
+    
 
     for i in range(100):
-        m = Maze(101, 0.3)
+        m = Maze(101, .33)
 
-        start = time.time()
-        ast.AStar(m, (0, 0), (100, 100), ast.mDistance, False)
-        end = time.time()
-        timeM += (end - start)
+        start = time.time() # time started
+        solve = ast.AStar(m, (0, 0), (100, 100), ast.mDistance, False)
+        while ~solve: # if maze isn't solvable then
+            m = Maze(101, .33) # recreate maze again
+            start = time.time() # restart timer
+            solve = ast.AStar(m, (0, 0), (100, 100), ast.mDistance, False)
+            if solve: # break when it finally is solvalbe
+                break
+        end = time.time() # time ended
+        timeM += (end - start) # total time ran
 
-    print(timeM / 100)
+    print(timeM / 100) #prints the run time
+    print(atl)
+    print(spDisGW)
+    print(spFullGW)
+    print(astarCells)
 
 if __name__ == "__main__":
     main()
