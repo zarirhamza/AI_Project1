338c2e04ffd78e32f52857d5c5b9424c09d6154b
pizzadeesnutz <arcadio@thearcadio.com>
2021-09-22 11:52:51 -0400

    Fixed Q6
    because astar is keeping track of the trajectory the total we were getting was already cumulative, but we were also keeping a total variable, so we were getting a cumulative sum of the cumulative sum and it was growing out of control really fast...
    
    also added a couple lines of code to reset it back to 0 for this question.
    
    Going to finish q7 tonight, saw your notes that you modified the code for that question, thanks a lot!

--- main2.py
+++ main2.py
@@ -1,18 +1,21 @@
 # Main file to test our code
-import time
+# import time # commented out in this script since it wasn't being used
 import Astar as ast
 from maze import Maze
 
 
 def main():
-    totalTraj = 0
-    totalCells = 0
+    # totalTraj = 0 # commented out here
+    # totalCells = 0
     totalFinal = 0
     totalFull = 0
     
     iterations = 100 # desired number of mazes to generate
-    density = 0.33 # likliehood of a cell being blocked
-
+    density = 0.315 # likliehood of a cell being blocked
+    # need to reset astar variables since previous iterations will be included
+    ast.trajectoryUnknown = 0
+    ast.cellsProcessed = 0
+    
     for i in range(iterations):
         m = Maze(101, density)
         
@@ -22,22 +25,19 @@ def main():
             if(ast.DFSSolve(m, (0,0), (100,100))): # break when solvable
                 break
 
-        resK = ast.AStarKnown(m, (0, 0), (100, 100), ast.mDistance, True)   
+        resK = ast.AStarKnown(m, (0, 0), (100, 100), ast.mDistance, True)
         resU, trajectory, cells = ast.AStarUnknown(m, (0, 0), (100, 100), ast.mDistance, True)
 
-        totalTraj += trajectory
-        totalCells += cells
         totalFinal += len(resU)
         totalFull += len(resK)
         
-        print(i)
+        print("Iteration: " + str(i+1))
 
     print("Density = " + str(density))
-    print("Average Trajectory = " + str(totalTraj/iterations))
-    print("Average Cells Processed = " + str(totalCells/iterations))
+    print("Average Trajectory Length = " + str(trajectory/iterations))
+    print("Average Cells Processed = " + str(cells/iterations))
     print("Path Length Final Gridworld = " + str(totalFinal/iterations))
     print("Path Length Full Gridworld = " + str(totalFull/iterations))
-
 
 if __name__ == "__main__":
     main()
